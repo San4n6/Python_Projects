611d2e638ea8fd6b9fb2cd9bc526ac50a809c50d
S@n4n6 <34033282+nccpd4n6@users.noreply.github.com>
2018-12-06 20:37:53 -0500

    Add files via upload

---
+++ S@n_cache_encryptedB_all.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+'''
+Copyright (c) 2018, S@n4n6 join us on Discord https://discord.gg/MaDteVY
+All rights reserved.
+'''
+
+import sqlite3
+import pandas as pd
+import datetime, pytz
+import csv
+
+conn = sqlite3.connect("cache_encryptedB.db")
+df = pd.read_sql_query("Select * from CellLocation", conn)
+
+df['Timestamp'] = pd.to_datetime(df.Timestamp + 978307200, unit ='s')
+
+df.to_csv('Cache_encryptedB.csv')
+
+conn.close()
+
+conn = sqlite3.connect("cache_encryptedB.db")
+df = pd.read_sql_query("Select * from LteCellLocation", conn)
+
+df['Timestamp'] = pd.to_datetime(df.Timestamp + 978307200, unit ='s')
+
+df.to_csv('LteCellLocation.csv')
+
+conn.close()
+
+conn = sqlite3.connect("cache_encryptedB.db")
+df = pd.read_sql_query("Select * from LteCellLocationLocal", conn)
+
+df['Timestamp'] = pd.to_datetime(df.Timestamp + 978307200, unit ='s')
+
+df.to_csv('LteCellLocationLocal.csv')
+
+conn.close()
+
+conn = sqlite3.connect("cache_encryptedB.db")
+df = pd.read_sql_query("Select * from WifiLocation", conn)
+
+df['Timestamp'] = pd.to_datetime(df.Timestamp + 978307200, unit ='s')
+
+df.to_csv('WifiLocation.csv')
+
+conn.close()
